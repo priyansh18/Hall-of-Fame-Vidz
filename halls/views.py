c1c30ad0e31905f271a277a14509ca7500da71c9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-03 22:34:22 +0530

    Authentication Views & Templates

--- halls/views.py
+++ halls/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def home(request):
     context = {}
     return render(request, 'halls/home.html', context)
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
