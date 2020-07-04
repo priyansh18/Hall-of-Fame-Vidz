6ec4e6fb45868508a390fd8c8f1a9e1d25b9ee8f
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-04 20:38:09 +0530

    AutoLogin after Signup

--- halls/views.py
+++ halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
 from .models import Hall
 # Create your views here.
 
@@ -14,6 +15,14 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self,form):
+        view = super(SignUp,self).form_valid(form)
+        print(view)
+        username,password = form.cleaned_data.get('username'),form.cleaned_data.get('password1')
+        user = authenticate(username=username,password=password)
+        login(self.request,user)
+        return view
 
 class CreateHall(CreateView):
     model=Hall
