dde845017b62bb1ff503ea2b3350360faa5f344a
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-04 22:24:34 +0530

    CRUD operation using ClassBasedView

--- halls/urls.py
+++ halls/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name="home"),
-    path('halloffame/create',views.CreateHall.as_view(), name='create_hall')
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('halloffame/create',views.CreateHall.as_view(), name='create_hall'),
+    path('halloffame/<int:pk>',views.DetailHall.as_view(), name='detail_hall'),
+    path('halloffame/<int:pk>/update',views.UpdateHall.as_view(), name='update_hall'),
+    path('halloffame/<int:pk>/delete',views.DeleteHall.as_view(), name='delete_hall'),
 ]
--- halls/views.py
+++ halls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
 from .models import Hall
 # Create your views here.
 
@@ -11,26 +11,53 @@ def home(request):
     context = {}
     return render(request, 'halls/home.html', context)
 
+
+def dashboard(request):
+    context = {}
+    return render(request, 'halls/dashboard.html', context)
+
+
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
-    def form_valid(self,form):
-        view = super(SignUp,self).form_valid(form)
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
         print(view)
-        username,password = form.cleaned_data.get('username'),form.cleaned_data.get('password1')
-        user = authenticate(username=username,password=password)
-        login(self.request,user)
+        username, password = form.cleaned_data.get(
+            'username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
         return view
 
-class CreateHall(CreateView):
-    model=Hall
-    fields=['title']
-    template_name = 'halls/create_hall.html'
-    success_url = reverse_lazy('home')
 
-    def form_valid(self,form):
+class CreateHall(CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
         form.instance.user = self.request.user
-        super(CreateHall,self).form_valid(form)
+        super(CreateHall, self).form_valid(form)
         return redirect('home')
+
+
+class DetailHall(DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+
+class UpdateHall(UpdateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/update_hall.html'
+    success_url = reverse_lazy('dashboard')
+
+
+class DeleteHall(DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'
+    success_url = reverse_lazy('dashboard')
+
